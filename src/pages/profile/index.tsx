f3b19c72751fd3ecd49fa5648e86227745b13f18
Daiki Yamamoto <0ey329231k1095v@gmail.com>
2023-10-30 08:46:32 -0700

    commented out /profile/index.tsx

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,50 +1,50 @@
-import React from 'react'
-import ProfilePage from './ProfilePage'
-import { supabase } from '@/lib/supabaseClient'
+// import React from 'react'
+// import ProfilePage from './ProfilePage'
+// import { supabase } from '@/lib/supabaseClient'
 
-const index = ({data}:{data:any}) => {
-  return (
-    <ProfilePage data={data} />
-  )
-}
+// const index = ({data}:{data:any}) => {
+//   return (
+//     <ProfilePage data={data} />
+//   )
+// }
 
-export default index
+// export default index
 
-export async function getStaticPaths() {
-  const {data, error} = await supabase
-    .from('users')
-    .select()
+// export async function getStaticPaths() {
+//   const {data, error} = await supabase
+//     .from('users')
+//     .select()
     
-    if(error){
-            console.log(error);
-            return []
-    }
+//     if(error){
+//             console.log(error);
+//             return []
+//     }
 
-  const allPaths = data.map((user) => {
-    return {
-      params: {
-        id: user.id.toString(),
-      },
-    };
-  });
-  return {
-    paths: allPaths,
-    fallback: false,
-  };
-}
+//   const allPaths = data.map((user) => {
+//     return {
+//       params: {
+//         id: user.id.toString(),
+//       },
+//     };
+//   });
+//   return {
+//     paths: allPaths,
+//     fallback: false,
+//   };
+// }
 
-export async function getStaticProps(context:any) {
-  console.log(context.params.id);
-  const {data, error} = await supabase
-    .from('users')
-    .select()
+// export async function getStaticProps(context:any) {
+//   console.log(context.params.id);
+//   const {data, error} = await supabase
+//     .from('users')
+//     .select()
     
-    if(error){
-            console.log(error);
-            return []
-    }
+//     if(error){
+//             console.log(error);
+//             return []
+//     }
 
-    const userData = data.filter(data => data.id === context.params.id)
+//     const userData = data.filter(data => data.id === context.params.id)
 
-  return {props: {data: userData}}
-}
+//   return {props: {data: userData}}
+// }
